332f958d7ffe4559208c2f421c55061e79775ac8
Pratham Shah <itspratham2911@gmail.com>
2026-07-22 00:20:27 +0530

    fix: resolve test package naming and configure H2 test database

--- src/test/java/com/prathamcodes/scattermerge/ScatterMergeApplicationTests.java
+++ src/test/java/com/prathamcodes/scattermerge/ScatterMergeApplicationTests.java
@@ -1,4 +1,4 @@
-package com.prathamcodes.ScatterMerge;
+package com.prathamcodes.scattermerge;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ScatterMergeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
