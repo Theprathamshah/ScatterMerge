def3a235b5a5184472d184598ccf3a56c6641501
Pratham Shah <itspratham2911@gmail.com>
2026-07-23 00:07:41 +0530

    feat(backend): :sparkles: add api mock and airline connectors

--- src/main/java/com/prathamcodes/scattermerge/config/HttpClientConfig.java
+++ src/main/java/com/prathamcodes/scattermerge/config/HttpClientConfig.java
@@ -1,8 +1,32 @@
 package com.prathamcodes.scattermerge.config;
 
+import java.net.http.HttpClient;
+import java.net.http.HttpClient.Redirect;
+import java.net.http.HttpClient.Version;
+import java.time.Duration;
+import java.util.concurrent.Executors;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 public class HttpClientConfig {
 
+    @Bean
+    public HttpClient httpClient() {
+        return HttpClient.newBuilder()
+            .version(Version.HTTP_2)
+            .connectTimeout(Duration.ofSeconds(5))
+            .followRedirects(Redirect.NORMAL)
+            .executor(Executors.newVirtualThreadPerTaskExecutor())
+            .build();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper()
+            .findAndRegisterModules();
+    }
 }
